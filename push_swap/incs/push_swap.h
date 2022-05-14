8a6397b78fa2a878a6da6c78abc3043211534620
Даниль <92218463+Byrabylka@users.noreply.github.com>
2022-05-14 21:31:27 +0300

    Add files via upload

--- push_swap/incs/push_swap.h
+++ push_swap/incs/push_swap.h
@@ -18,8 +18,12 @@
 # include <fcntl.h>
 #include <stdio.h>
 
-#include "../libft/libft.h"
 
+typedef struct s_stack
+{
+	int			content;
+	struct s_stack	*next;
+}	t_stack;
 
 typedef struct s_data
 {
@@ -41,12 +45,28 @@ t_data	*init_data(t_data *data, char **argv, int argc);
 void	find_min_max(t_data *data, int p);
 void	find_median(t_data *data, int p);
 int		fill_stack_a(t_data *data);
-void	solve(t_data *data);
-void	solve_5_or_less(t_data *data);
+void	solve(t_data *data, int n);
+void	solve_5_or_less(t_data *data, int p);
+void 	solve_5_or_less_b(t_data *data);
 long	long_atoi(char *str);
 void	free_data(t_data *data);
-int		is_sorted(t_data *data);
+t_stack	*ft_predlast(t_stack *lst);
+int		is_sorted(t_stack *stack);
+int		is_sorted_b(t_stack *stack);
 void	global_solve(t_data *data);
+
+int		ft_lstsize(t_stack *lst);
+t_stack	*ft_lstlast(t_stack *lst);
+void	ft_lstadd_front(t_stack **lst, t_stack *new);
+void	ft_lstadd_back(t_stack **lst, t_stack *new);
+t_stack	*ft_lstnew(int content);
+
+
+void	ft_putstr_fd(char *s, int fd);
+int		ft_isdigit(int ch);
+char	**ft_split(char const *s, char c);
+int		ft_strlen(const char *str);
+void	ft_lstclear(t_stack **lst);
 
 void	swap_a(t_data *data, int p);
 void	swap_b(t_data *data, int p);
@@ -59,4 +79,10 @@ void	r_rotate(t_data *data);
 void	r_rotate_a(t_data *data, int p);
 void	r_rotate_b(t_data *data, int p);
 void	r_r_rotate(t_data *data);
+
+void	swap_stack(t_data *data, int p);
+void	rotate_stack(t_data *data, int p);
+void	r_rotate_stack(t_data *data, int p);
+
+void ft_out(t_stack *a);
 #endif
