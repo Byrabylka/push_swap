b0077d61884744d27ad1b172dfa748395aebdf44
Даниль <92218463+Byrabylka@users.noreply.github.com>
2022-07-10 20:30:31 +0300

    Delete push_swap directory

--- push_swap/1/color.py
+++
@@ -1,5 +0,0 @@
-from secrets import randbelow
-
-
-import random as rand
-print(rand.randint(1, 100))
--- push_swap/1/rand.py
+++
@@ -1,6 +0,0 @@
-import random as rand
-a = list(range(100000))
-rand.shuffle(a)
-print("./push_swap ", end = ' ')
-for i in a:
-	print(i, end = ' ')
